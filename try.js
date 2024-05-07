839b377eef3cc2d7f8ff9cdb604ce758ede81da5
sam <saminusama@gmail.com>
2024-05-06 20:22:59 -0400

    process.env

--- try.js
+++ try.js
@@ -5,12 +5,12 @@ import cors from "cors";
 
 const express = Express();
 
-const housesCol = await houses();
-const house1 = await housesCol.findOne();
+// const housesCol = await houses();
+// const house1 = await housesCol.findOne();
 // console.log(await housesCol.findOne());
 
 express.use(cors());
-express.use("/", (req, res) => res.json(house1));
+express.use("/", (req, res) => res.json(process.env));
 
 express.listen(3000, () => {
   console.log("running on http://localhost:3000");
