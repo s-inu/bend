d1e456a739db794255b33d5534d65150c156c5cf
sam <saminusama@gmail.com>
2024-05-06 20:05:07 -0400

    cors

--- try.js
+++ try.js
@@ -1,5 +1,17 @@
 import { houses } from "./mongoCollections.js";
 import "dotenv/config";
+import Express from "express";
+import cors from "cors";
+
+const express = Express();
 
 const housesCol = await houses();
-console.log(await housesCol.findOne());
+const house1 = await housesCol.findOne();
+// console.log(await housesCol.findOne());
+
+express.use(cors());
+express.use("/", (req, res) => res.json(house1));
+
+express.listen(3000, () => {
+  console.log("running on http://localhost:3000");
+});
